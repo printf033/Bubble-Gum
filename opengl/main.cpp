4bcce3bb15724f911264658ca4fb97a805ae9ae7
aaa <printf033@gmail.com>
2025-04-01 18:27:37 +0800

    debug

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -22,10 +22,19 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void mouse_callback(GLFWwindow *window, double xpos, double ypos);
-void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.processKeyboard(CamMovement::FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.processKeyboard(CamMovement::BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.processKeyboard(CamMovement::LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.processKeyboard(CamMovement::RIGHT, deltaTime);
+}
 
 int main()
 {
@@ -38,6 +47,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true); // debug
 	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "BBG", NULL, NULL);
 	if (window == nullptr)
 	{
@@ -46,9 +56,23 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width, int height)
+								   { glViewport(0, 0, width, height); });
+	glfwSetCursorPosCallback(window, [](GLFWwindow *window, double xpos, double ypos)
+							 {
+		if (firstMouse)
+		{
+			lastX = xpos;
+			lastY = ypos;
+			firstMouse = false;
+		}
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos;
+		lastX = xpos;
+		lastY = ypos;
+		camera.processMouseMovement(xoffset, yoffset); });
+	glfwSetScrollCallback(window, [](GLFWwindow *window, double xoffset, double yoffset)
+						  { camera.processMouseScroll(yoffset); });
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -56,6 +80,101 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+	//////////////////////////////////////////////////////////////////// debug
+	GLint flags;
+	glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(
+			[](GLenum source, GLenum type, GLuint id, GLenum severity,
+			   GLsizei length, const GLchar *message, const void *userParam)
+			{
+				// 忽略一些不重要的错误/警告代码
+				if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
+					return;
+
+				std::cout << "---------------" << std::endl;
+				std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+				switch (source)
+				{
+				case GL_DEBUG_SOURCE_API:
+					std::cout << "Source: API";
+					break;
+				case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+					std::cout << "Source: Window System";
+					break;
+				case GL_DEBUG_SOURCE_SHADER_COMPILER:
+					std::cout << "Source: Shader Compiler";
+					break;
+				case GL_DEBUG_SOURCE_THIRD_PARTY:
+					std::cout << "Source: Third Party";
+					break;
+				case GL_DEBUG_SOURCE_APPLICATION:
+					std::cout << "Source: Application";
+					break;
+				case GL_DEBUG_SOURCE_OTHER:
+					std::cout << "Source: Other";
+					break;
+				}
+				std::cout << std::endl;
+
+				switch (type)
+				{
+				case GL_DEBUG_TYPE_ERROR:
+					std::cout << "Type: Error";
+					break;
+				case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+					std::cout << "Type: Deprecated Behaviour";
+					break;
+				case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+					std::cout << "Type: Undefined Behaviour";
+					break;
+				case GL_DEBUG_TYPE_PORTABILITY:
+					std::cout << "Type: Portability";
+					break;
+				case GL_DEBUG_TYPE_PERFORMANCE:
+					std::cout << "Type: Performance";
+					break;
+				case GL_DEBUG_TYPE_MARKER:
+					std::cout << "Type: Marker";
+					break;
+				case GL_DEBUG_TYPE_PUSH_GROUP:
+					std::cout << "Type: Push Group";
+					break;
+				case GL_DEBUG_TYPE_POP_GROUP:
+					std::cout << "Type: Pop Group";
+					break;
+				case GL_DEBUG_TYPE_OTHER:
+					std::cout << "Type: Other";
+					break;
+				}
+				std::cout << std::endl;
+
+				switch (severity)
+				{
+				case GL_DEBUG_SEVERITY_HIGH:
+					std::cout << "Severity: high";
+					break;
+				case GL_DEBUG_SEVERITY_MEDIUM:
+					std::cout << "Severity: medium";
+					break;
+				case GL_DEBUG_SEVERITY_LOW:
+					std::cout << "Severity: low";
+					break;
+				case GL_DEBUG_SEVERITY_NOTIFICATION:
+					std::cout << "Severity: notification";
+					break;
+				}
+				std::cout << std::endl;
+				std::cout << std::endl;
+			},
+			nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+	}
+	//////////////////////////////////////////////////////////////////// debug
 	glEnable(GL_DEPTH_TEST);
 	Shader shader(std::filesystem::current_path() / "../opengl/model_vs.glsl",
 				  std::filesystem::current_path() / "../opengl/model_fs.glsl");
@@ -87,38 +206,4 @@ int main()
 	glfwTerminate();
 	return 0;
 }
-void processInput(GLFWwindow *window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.processKeyboard(CamMovement::FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.processKeyboard(CamMovement::BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.processKeyboard(CamMovement::LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.processKeyboard(CamMovement::RIGHT, deltaTime);
-}
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-void mouse_callback(GLFWwindow *window, double xpos, double ypos)
-{
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos;
-	lastX = xpos;
-	lastY = ypos;
-	camera.processMouseMovement(xoffset, yoffset);
-}
-void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
-{
-	camera.processMouseScroll(yoffset);
-}
+
